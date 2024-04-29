e905f2cd5f9db4c996875e4db8d4be0183493184
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2024-04-29 13:50:58 +0400

    update

--- SecondSemester/YAP/Lab4/lab4_1.cpp
+++ SecondSemester/YAP/Lab4/lab4_1.cpp
@@ -1,24 +1,35 @@
 #include<iostream>
 #include<fstream>
-
+//лист дерева
 struct Leafs
 {
-    int val;
+    //значение
+    long long val;
+    //левый и правый потомки
     Leafs* l;
     Leafs* r;
-
+    //получение высоты
     int GetsHieght();
+    //конструктор
+    Leafs(long long c);
 };
-
+//констурктор
+Leafs::Leafs(long long c) {
+    this->val = c;
+    this->l = nullptr;
+    this->r = nullptr;
+}
+//функция для получения высоты
 int Leafs::GetsHieght() {
     int lev, prav;
+    //проверяем левого потомка
     if (l) {
         lev = l->GetsHieght();
     }
     else {
         lev = 0;
     }
-
+    //проверяем правого потомка
     if (r) {
         prav = r->GetsHieght();
     }
@@ -28,9 +39,11 @@ int Leafs::GetsHieght() {
 
     return 1 + std::min(lev, prav);
 }
-
+//добавление новых элементов в дерево
 void AddsNew(Leafs*& root, Leafs* new_elem) {
+    //если корень не пустой
     if (root) {
+        //согласно сбалансированному дерево вставляем элементы и вызываем рекурсивно функции
         if (root->l == nullptr) {
             root->l = new_elem;
         }
@@ -44,28 +57,86 @@ void AddsNew(Leafs*& root, Leafs* new_elem) {
             AddsNew(root->r, new_elem);
         }
     }
+    //если корень пустой
+    else {
+        root = new_elem;
+    }
 
 }
+//фунция для отражения значения элементов
+long long rev(long long v) {
+    long long n = std::abs(v);
+    long long result = 0;
+    //переворачиваем
+    while (n > 0) {
+        result *= 10;
+        result += n % 10;
+        n /= 10;
+    }
+    //проверка на отрицательное
+    if (v < 0) {
+        result *= -1;
+    }
+    return result;
+}
+//фукнция для отраженения значений элементов дерева
+void OborotElem(Leafs*& root)
+{
+    if (root){
+        root->val = rev(root->val);
 
+        if (root->l) {
+            OborotElem(root->l);
+        }
+        if (root->r) {
+            OborotElem(root->r);
+        }
+    }
+}
+//функция создания нового дерева
+void CreateTree(std::ifstream& in, Leafs*& root) {
 
-void CreateTree(std::ifstream& in,Leafs*& root){
-
-    int v;
-    //считываем поочередно элементы
-    while (in>>v)
+    long long v;
+    //считываем значения
+    while (in >> v)
     {
-        //новый элемент
+        //создаем элемент
         Leafs* new_node = new Leafs(v);
-        //запускаем рекурсию
-        AddNewNode(root,new_node);
+        //рекурсия
+        AddsNew(root, new_node);
+    }
+}
+//симметричный вывод
+void out(std::ofstream& exit, Leafs*& r) {
+    if (r) {
+        //рекурсия для левого потомка
+        out(exit,r->l);
+        //вывод в консоль
+        std::cout << r->val << ' '; 
+        //вывод в файл
+        exit << r->val << ' ';
+        //рекусрия для правого потомка
+        out(exit, r->r);
+        //очистка памяти
+        delete r;
     }
 }
 
 
 
-
 int main() {
-
-
-
+    //открываем файлы
+    std::ifstream in("input.txt");
+    std::ofstream exit("output.txt");
+    //создаем корень дерева
+    Leafs* head = nullptr;
+    //считываем дерево
+    CreateTree(in, head);
+    //выполняем отражение значений элементов дерева
+    OborotElem(head);
+    //симметричный обход
+    out(exit,head);
+    //закрываем файлы
+    in.close();
+    exit.close();
 }
